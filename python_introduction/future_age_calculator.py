29e9fa35c40fb5ef4cade8073e65c18e1740a7cb
Abdulmenan Amin <abdullahtate3@gmail.com>
2025-11-08 19:06:19 +0300

    Add python introduction project

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+current_age = input('How old are you?')
+current_year = 2023
+age = int(current_age) + 27 
+print('In 2050, you will be', age,'years old.')
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 2
+one_hour_minutes = 60 
+one_minute_seconds = 60
+one_hour_seconds = one_hour_minutes * one_minute_seconds
+seconds = hours * one_hour_seconds
+print(hours,'hour(s) is', seconds,'seconds')
