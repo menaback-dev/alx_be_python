95a1e9d77fd6b5b06d94bbe9f73c7573a22400a0
Abdulmenan Amin <abdullahtate3@gmail.com>
2025-11-23 20:22:22 +0300

    Fix divide by zero scenario

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,6 +12,8 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 print("Cannot divide by zero.")
-            else:
+            elif num1 == int:
                 result = num1 / num2
                 return result
+            else:
+                print("invalid")
