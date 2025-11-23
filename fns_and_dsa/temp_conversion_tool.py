5fba803ff90d495dc71cd8080aed60fd29c51830
Abdulmenan Amin <abdullahtate3@gmail.com>
2025-11-23 20:36:24 +0300

    Fix the handling of value error in temp conversion file

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,6 +3,8 @@ celsius_to_fahrenheit_factor = 9/5
 
 def convert_to_celsius():
     fahrenheit = float(input("Enter the temperature to convert: "))
+    if fahrenheit != float:
+        print("Invalid temperature. Please enter a numeric value.")
     unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
     if unit == "f":
         result = (fahrenheit - 32) * fahrenheit_to_celsius_factor
@@ -13,6 +15,8 @@ convert_to_celsius()
 
 def convert_to_fahrenheit():
     celsius = float(input("Enter the temperature to convert: "))
+    if celsius != float:
+        print("Invalid temperature. Please enter a numeric value.")
     unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
     if unit == "c":
         result = (celsius * celsius_to_fahrenheit_factor) + 32
