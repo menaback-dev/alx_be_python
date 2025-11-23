1e20446c5c9d5f585274bdb9d558536d84c25e5f
Abdulmenan Amin <abdullahtate3@gmail.com>
2025-11-23 20:25:51 +0300

    Fix the implementation of choice in shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item_name = input("Enter the item name: ")
+            item_name = input("Enter the item to add: ")
             shopping_list.append(item_name)
             pass
         elif choice == '2':
